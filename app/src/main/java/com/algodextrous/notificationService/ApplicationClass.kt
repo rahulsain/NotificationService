26d2ccba28071bf1bc937c829057419b8d10d4b2
rahulsain <50570677+rahulsain@users.noreply.github.com>
2023-06-15 23:01:37 +0530

    first commit

---
+++ app/src/main/java/com/algodextrous/notificationService/ApplicationClass.kt
@@ -0,0 +1,9 @@
+package com.algodextrous.notificationService
+
+import android.app.Application
+
+class ApplicationClass : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
